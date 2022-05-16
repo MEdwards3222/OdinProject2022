c3be31f24d296ba29603111bd152010cd8db1829
MEdwards3222 <medwards3222@gmail.com>
2022-05-16 19:49:00 -0400

    Completed RPS - conducting tests

--- rock-paper-scissors/RPS.js
+++ rock-paper-scissors/RPS.js
@@ -71,9 +71,100 @@ function playRound(playerSelection, computerSelection) {
         }
     }
 
+    if (playerSelection == "paper") {
+        if (computerSelection == "rock") {
+            alert(`Computer chose: ${computerSelection}. You win!`);
+            return 1;
+        }
+
+        else if (computerSelection == "scissors") {
+            alert(`Computer chose: ${computerSelection}. You lose...`);
+            return -1;
+        }
+
+        else if (computerSelection == "paper") {
+            alert(`Computer chose: ${computerSelection}. It's a tie...!`);
+            return 0;
+        }
+
+        else {
+            alert("Something went wrong... Sorry, errors count as ties! It's totally a feature, not a bug.");
+            return 0;
+        }
+    }
+
+    if (playerSelection == "scissors") {
+        if (computerSelection == "paper") {
+            alert(`Computer chose: ${computerSelection}. You win!`);
+            return 1;
+        }
+
+        else if (computerSelection == "rock") {
+            alert(`Computer chose: ${computerSelection}. You lose...`);
+            return -1;
+        }
+
+        else if (computerSelection == "scissors") {
+            alert(`Computer chose: ${computerSelection}. It's a tie...!`);
+            return 0;
+        }
+
+        else {
+            alert("Something went wrong... Sorry, errors count as ties! It's totally a feature, not a bug.");
+            return 0;
+        }
+    }
+
+    return console.log("Play round failed to execute properly!");
 }
 
 //Create a game - a game consists of 5 rounds. Whoever has the highest score wins.
 function game() {
+let playerScore = 0;
+let comScore = 0;
+let tie = 0;
 
+    for (i = 0; i < 5; i++) {
+        let result = 0;
+
+        console.log(`Beginning round ${i}!`);
+        playerChoice = playerSelection();
+        comChoice = computerPlay();
+
+        result = playRound(playerChoice, comChoice);
+
+        if (result == 1){
+            playerScore++;
+        }
+
+        if (result == -1) {
+            comScore++;
+        }
+
+        if (result == 0) {
+            tie++;
+        }
+
+        alert(`Current Score: \n\t Player Score: ${playerScore}\n\t Computer Score: ${comScore}\n\t Total Ties: ${tie}`);
+    }
+
+    if (playerScore > comScore) {
+        alert(`The current Score: \n\t Player Score: ${playerScore}\n\t Computer Score: ${comScore}\n\t Total Ties: ${tie}\n\n You win!!`);
+        alert("Game over! Thank you for playing!");
+        return 0;
+    }
+
+    else if (comScore > playerScore) {
+        alert(`The current Score: \n\t Player Score: ${playerScore}\n\t Computer Score: ${comScore}\n\t Total Ties: ${tie}\n\n You lose, better luck next time!`);
+        alert("Game over! Thank you for playing!");
+        return 0;
+    }
+
+    else if (playerScore == comScore) {
+        alert(`The current Score: \n\t Player Score: ${playerScore}\n\t Computer Score: ${comScore}\n\t Total Ties: ${tie}\n\n It's a tie! Such fierce competition!`);
+        alert("Game over! Thank you for playing!");
+        return 0;
+    }
 }
+
+game();
