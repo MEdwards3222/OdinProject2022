876cffbaa951c1663cd741a33e4c00ac61299513
Michael Edwards <MEdwards3222@gmail.com>
2022-08-04 18:14:18 -0400

    Created calc project - created html and partially completed JS

---
+++ calculator/calc.js
@@ -0,0 +1,6 @@
+let calcContainer = document.querySelector('.calculator');
+let calcDisplay = document.querySelector('#display');
+let displayTotal = " ";
+let runningTotal = 0;
+let operator = " ";
+let btn = document.querySelector('.Buttons');
